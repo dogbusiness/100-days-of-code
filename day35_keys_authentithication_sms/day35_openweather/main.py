db75c99f898aedce065fb6ca91e0b41b36ba9ba9
Serge <94310876+dogbusiness@users.noreply.github.com>
2022-07-24 22:27:53 +0300

    Add files via upload
    Added to telegram bot

---
+++ day35_keys_authentithication_sms/day35_openweather/main.py
@@ -0,0 +1,25 @@
+import requests
+import os
+# one_call_api_request = 'https://api.openweathermap.org/data/2.5/onecall'
+# prepod_api_key = '69f04e4613056b159c2761a9d9e664d2'
+parameters = {
+    'appid': '97b62f37dd9ad7b18b23c2e0589348bf',
+    'lon': 39.17,
+    'lat': 51.6664,
+    'exclude': 'current,minutely',
+    'units': 'metric'
+}
+
+# Sending request
+response = requests.get(url='https://api.openweathermap.org/data/2.5/onecall', params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data)
+
+# Forming twelve hours forecast
+twelve_hours = [weather_data['hourly'][i]['weather'][0]['id'] for i in range(0, 11)]
+print(twelve_hours)
+# for item in twelve_hours:
+#     if item < 803:
+#         print('Clear Sky')
+
